8f6198f7e654c291c1d4985f7eb9a8c1b2f57313
dev1 <jtorrealba3@gmail.com>
2018-04-03 11:53:28 -0400

    Add Test stage2

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+!/usr/bin/env sh
 
 echo 'The following "npm" command (if executed) installs the "cross-env"'
 echo 'dependency into the local "node_modules" directory, which will ultimately'
@@ -10,7 +10,7 @@ echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
-# npm install --save-dev cross-env
+ npm install --save-dev cross-env
 set +x
 
 echo 'The following "npm" command tests that your simple Node.js/React'
